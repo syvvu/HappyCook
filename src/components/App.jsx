1b4e3d6d8033a314d2853032062d556ff088485c
syvvu <sswu@ucsd.edu>
2023-09-12 13:58:11 -0700

    Handle client-side routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SelectItems from "./selectItems/SelectItems";
 import SelectTab from "./selectTab/SelectTab";
 
 function App() {
   return (
-    <div>
-      <SelectTab />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/ingredients" element={<SelectItems />} />
+        <Route path="/recipes" element={<SelectTab />} />
+        <Route path="/" element={<SelectItems />} />
+      </Routes>
+    </Router>
   );
 }
 
