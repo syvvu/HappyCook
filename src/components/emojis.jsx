6e05a9408873301aae6f26dd89145ba41649dfa1
syvvu <sswu@ucsd.edu>
2023-09-18 22:00:45 -0700

    Update emojis list

--- src/components/emojis.jsx
+++ src/components/emojis.jsx
@@ -1,11 +1,73 @@
+const proteins = {
+  Chicken: "🍗",
+  Beef: "🥩",
+  Fish: "🐟",
+  Ham: "🍖",
+  Pork: "🥓",
+  Sausage: "🌭️",
+  Shrimp: "🍤",
+  Tofu: "🍲",
+  Egg: "🥚",
+};
+
+const vegetables = {
+  Broccoli: "🥦",
+  Carrot: "🥕",
+  Corn: "🌽",
+  Cucumber: "🥒",
+  Garlic: "🧄",
+  Ginger: "🫚",
+  Onion: "🧅",
+  "Bell Pepper": "🫑",
+  "Leafy Green": "🥬",
+  Potato: "🥔",
+  "Sweet Potato": "🍠",
+  Tomato: "🍅",
+  Mushroom: "🍄",
+};
+
+const grains = {
+  Bread: "🍞",
+  Rice: "🍚",
+  Spaghetti: "🍝",
+  Noodle: "🍜",
+  Pancake: "🥞",
+  Cereal: "🥣",
+};
+
+const dairy = {
+  Milk: "🥛",
+  Butter: "🧈",
+  Cheese: "🧀",
+  Cream: "🍦",
+};
+
+/**
+const fruits = {
+  Apple: "🍎",
+  Pear: "🍐",
+  Orange: "🍊",
+  Banana: "🍌",
+  Watermelon: "🍉",
+  Grapes: "🍇",
+  Blueberries: "🫐",
+  Strawberry: "🍓",
+  Lemon: "🍋",
+  Cherry: "🍒",
+  Peach: "🍑",
+  Mango: "🥭",
+  Pineapple: "🍍",
+  Coconut: "🥥",
+  Kiwi: "🥝",
+  Avocado: "🥑",
+};
+*/
+
 const emojis = {
-    Chicken: "🍗",
-    Garlic: "🧄",
-    Broccoli: "🥦",
-    Onion: "🧅",
-    Mushroom: "🍄",
-    Cream: "🧈",
-    // ... more mappings
-  };
-  
-  export default emojis;
+  ...proteins,
+  ...vegetables,
+  ...grains,
+  ...dairy,
+};
+
+export default emojis;
