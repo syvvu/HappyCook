780b17e9871b22644744cb207f9f54c8caaf35d2
syvvu <sswu@ucsd.edu>
2023-10-04 12:12:02 -0700

    Bind to dynamic port for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const findRecipes = require("./findRecipes");
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
